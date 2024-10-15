69d696d08345cfe851099570dedce55c3113fd19
usmanqt <qureshiu@mail.gvsu.edu>
2024-10-15 15:22:09 -0400

    commit with configs secrets no longer visible

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import axios from "axios";
 import router from "./router";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import firebaseConfig from "../configs/firebase";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication
+const auth = getAuth(app);
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
